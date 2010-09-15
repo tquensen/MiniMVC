d2953a6f3c3e7e4d91cace4ee00998c9f412cf20
ScaraX <t.quensen@yeebase.com>
2010-09-15 19:49:45 +0200

    fixed user module route rights

--- module/User/settings/routes.php
+++ module/User/settings/routes.php
@@ -5,33 +5,33 @@ $MiniMVC_routes['user.register'] = array(
     'controller' => 'User_Register',
     'action' => 'register',
     'parameter' => array(),
-    'rights' => $rights->getRoleRights($rights->getRoleByKeyword('guest'))
+    'rights' => $rights->getRights('guest')
 );
 $MiniMVC_routes['user.login'] = array(
     'route' => 'user/login',
     'controller' => 'User_Login',
     'action' => 'login',
     'parameter' => array(),
-    'rights' => $rights->getRoleRights($rights->getRoleByKeyword('guest'))
+    'rights' => $rights->getRights('guest')
 );
 $MiniMVC_routes['user.logout'] = array(
     'route' => 'user/logout',
     'controller' => 'User_Login',
     'action' => 'logout',
     'parameter' => array(),
-    'rights' => $rights->getRoleRights($rights->getRoleByKeyword('user'))
+    'rights' => $rights->getRights('user')
 );
 $MiniMVC_routes['user.edit'] = array(
     'route' => 'user/edit',
     'controller' => 'User_Edit',
     'action' => 'edit',
     'parameter' => array(),
-    'rights' => $rights->getRoleRights($rights->getRoleByKeyword('user'))
+    'rights' => $rights->getRights('user')
 );
 $MiniMVC_routes['user.editPassword'] = array(
     'route' => 'user/editPassword',
     'controller' => 'User_Edit',
     'action' => 'editPassword',
     'parameter' => array(),
-    'rights' => $rights->getRoleRights($rights->getRoleByKeyword('user'))
+    'rights' => $rights->getRights('user')
 );
