ae681b49b9da451bd4a4a13248903c714028e7a5
ScaraX <t.quensen@yeebase.com>
2010-09-19 16:09:39 +0200

    added button form element

---
+++ lib/MiniMVC/Form/Element/Button.php
@@ -0,0 +1,14 @@
+<?php
+class MiniMVC_Form_Element_Button extends MiniMVC_Form_Element
+{
+	protected $type = 'submit';
+
+	public function setValue($value)
+	{
+	}
+
+	public function updateRecord()
+	{
+	}
+
+}
