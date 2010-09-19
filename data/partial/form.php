b588e43f59db9f848870beb79ab646ef82dbe1d8
ScaraX <t.quensen@yeebase.com>
2010-09-19 15:35:19 +0200

    updated forms

--- data/partial/form.php
+++ data/partial/form.php
@@ -10,7 +10,7 @@
         <li id="<?php echo htmlspecialchars($form->getName())?>__<?php echo htmlspecialchars($currentElement->getName())?>__wrapper" class="form<?php echo ucfirst($currentElement->getType())?>Wrapper<?php if (!$currentElement->isValid()):?> invalid<?php elseif($form->wasSubmitted): ?> valid<?php endif; ?><?php if ($currentElement->class): ?> <?php echo $currentElement->class; ?><?php endif; ?>">
         <?php endif; ?>
     <?php echo $this->get('form/' . $currentElement->getType(), array('element' => $currentElement)); ?>
-    <?php if (!$currentElement->info):?><span class="formInfo"><?php echo htmlspecialchars($currentElement->info)?></span><?php endif; ?>
+    <?php if ($currentElement->info):?><span class="formInfo"><?php echo htmlspecialchars($currentElement->info)?></span><?php endif; ?>
     <?php if (!$currentElement->isValid()):?><span class="formError"><?php echo htmlspecialchars($currentElement->errorMessage)?></span><?php endif; ?>
         <?php if ($currentElement->getType() != 'fieldset'): ?>
         </li>
