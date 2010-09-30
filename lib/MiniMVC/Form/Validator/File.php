93134f9ff588d6043b0aaa7b4d675e16ef7cb126
ScaraX <t.quensen@yeebase.com>
2010-09-30 21:52:56 +0200

    fixed file validator

--- lib/MiniMVC/Form/Validator/File.php
+++ lib/MiniMVC/Form/Validator/File.php
@@ -3,7 +3,7 @@ class MiniMVC_Form_Validator_File extends MiniMVC_Form_Validator
 {
 	public function validate($element, $value)
 	{
-		if ($element->fileError && ($this->required || $element->fileError == UPLOAD_ERR_NO_FILE)) {
+		if ($element->fileError && ($this->required || $element->fileError != UPLOAD_ERR_NO_FILE)) {
             return false;
         }
 
