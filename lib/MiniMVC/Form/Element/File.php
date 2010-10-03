83809939acd286cd2ad78ab250e32eb851f3f72c
ScaraX <t.quensen@yeebase.com>
2010-10-03 14:20:41 +0200

    updated file element and validator

--- lib/MiniMVC/Form/Element/File.php
+++ lib/MiniMVC/Form/Element/File.php
@@ -7,22 +7,34 @@ class MiniMVC_Form_Element_File extends MiniMVC_Form_Element
 	{
         parent::setValue(null);
 
+        $value = array();
         if (isset($_FILES[$this->getForm()->getName()])) {
             if (isset($_FILES[$this->getForm()->getName()]['name'][$this->name])) {
-                $this->fileName = $_FILES[$this->getForm()->getName()]['name'][$this->name];
+                $value['name'] = $_FILES[$this->getForm()->getName()]['name'][$this->name];
+            } else {
+                $value['name'] = null;
             }
             if (isset($_FILES[$this->getForm()->getName()]['tmp_name'][$this->name])) {
-                $this->fileTempName = $_FILES[$this->getForm()->getName()]['tmp_name'][$this->name];
+                $value['tmp_name'] = $_FILES[$this->getForm()->getName()]['tmp_name'][$this->name];
+            } else {
+                $value['tmp_name'] = null;
             }
             if (isset($_FILES[$this->getForm()->getName()]['type'][$this->name])) {
-                $this->fileType = $_FILES[$this->getForm()->getName()]['type'][$this->name];
-            }
-            if (isset($_FILES[$this->getForm()->getName()]['size'][$this->name])) {
-                $this->fileSize = $_FILES[$this->getForm()->getName()]['size'][$this->name];
+                $value['type'] = $_FILES[$this->getForm()->getName()]['type'][$this->name];
+            } else {
+                $value['type'] = null;
             }
             if (isset($_FILES[$this->getForm()->getName()]['error'][$this->name])) {
-                $this->fileError = $_FILES[$this->getForm()->getName()]['error'][$this->name];
+                $value['error'] = $_FILES[$this->getForm()->getName()]['error'][$this->name];
+            } else {
+                $value['error'] = null;
             }
+            if (isset($_FILES[$this->getForm()->getName()]['name'][$this->name])) {
+                $value['name'] = $_FILES[$this->getForm()->getName()]['name'][$this->name];
+            } else {
+                $value['name'] = null;
+            }
+            $this->value = $value;
         }
 	}
 }
--- lib/MiniMVC/Form/Validator/File.php
+++ lib/MiniMVC/Form/Validator/File.php
@@ -3,13 +3,18 @@ class MiniMVC_Form_Validator_File extends MiniMVC_Form_Validator
 {
 	public function validate($element, $value)
 	{
-		if ($element->fileError && ($this->required || $element->fileError != UPLOAD_ERR_NO_FILE)) {
+        if (!is_array($value)) {
             return false;
         }
 
-        if (file_exists($element->fileTempName)) {
-            $element->value = $element->fileTempName;
+		if ($value['error'] && ($this->required || $value['error'] != UPLOAD_ERR_NO_FILE)) {
+            return false;
         }
+
+        if (!file_exists($value['tmp_name'])) {
+            return false;
+        }
+        
         return true;
 	}
 }
