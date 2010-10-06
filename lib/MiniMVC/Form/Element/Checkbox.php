6f41d58505c5be2982f9a869e270192b9e4fa7f2
ScaraX <t.quensen@yeebase.com>
2010-10-06 16:04:42 +0200

    fixed checkbox form element

--- lib/MiniMVC/Form/Element/Checkbox.php
+++ lib/MiniMVC/Form/Element/Checkbox.php
@@ -2,4 +2,10 @@
 class MiniMVC_Form_Element_Checkbox extends MiniMVC_Form_Element
 {
 	protected $type = 'checkbox';
+
+    public function setValue($value)
+	{
+        parent::setValue((bool) $value);
+    }
+
 }
