a38f379f73e05b84ff224d5bc861f4ffd3bd72e0
tquensen <t.quensen@t3n.de>
2010-10-15 16:52:00 +0200

    added OR and AND validators

---
+++ lib/MiniMVC/Form/Validator/And.php
@@ -0,0 +1,26 @@
+<?php
+class MiniMVC_Form_Validator_And extends MiniMVC_Form_Validator
+{
+    protected $validators = array();
+
+    public function __construct($validators = array(), $options = array())
+	{
+        $this->validators = $validators;
+		$this->options = $options;
+
+        foreach ($this->validators as $validator) {
+            $validator->setElement($this->getElement());
+        }
+	}
+
+	public function validate($value)
+	{
+        foreach ($this->validators as $validator)
+        {
+            if (!$validator->validate($value)) {
+                return false;
+            }
+        }
+		return true;
+	}
+}
