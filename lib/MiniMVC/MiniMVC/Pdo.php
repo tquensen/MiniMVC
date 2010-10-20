6a1e93d9d3859583ab2055b68c6e1bf19a4c55c4
ScaraX <t.quensen@yeebase.com>
2010-10-20 23:44:48 +0200

    fixed db logging

--- lib/MiniMVC/MiniMVC/Pdo.php
+++ lib/MiniMVC/MiniMVC/Pdo.php
@@ -110,7 +110,7 @@ class LoggablePDO extends PDO
             foreach ((array) $log[1] as $key => $value) {
                 $params .= $key.': '.$value.'<br />';
             }
-            $data .= '<tr><td>'.$log[0].'</td><td>'.$params.'</td><td>'.$log[2].'s</td></tr>';
+            $data .= '<tr><td>'.$log[0].'</td><td>'.$params.'</td><td>'.$log[2].'ms</td></tr>';
         }
         $data .= '</table>';
 
