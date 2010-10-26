09ea8b38d442ae8b44bf636da39bfe58fd7125f6
tquensen <t.quensen@t3n.de>
2010-10-26 15:47:20 +0200

    updated htaccess and removed hashPrefix setting for static helper per default

--- lib/MiniMVC/data/settings/view.php
+++ lib/MiniMVC/data/settings/view.php
@@ -4,4 +4,4 @@ $MiniMVC_view['js'] = array();
 $MiniMVC_view['meta']['titleSeparator'] = ' | ';
 $MiniMVC_view['meta']['titleAlign'] = 'rtl';
 
-$MiniMVC_view['static']['prefixHash'] = true;
+//$MiniMVC_view['static']['prefixHash'] = true;
