e6ef5e68bd3dd0106b8e484025d9e58c16c5e377
ScaraX <t.quensen@yeebase.com>
2010-11-25 18:59:41 +0100

    fixed cli.php

--- cli.php
+++ cli.php
@@ -5,8 +5,10 @@ if (!isset($_SERVER['argv']) || count($_SERVER['argv']) < 2) {
 }
 
 include dirname(__FILE__).'/bootstrap.php';
+include MINIMVCPATH.'MiniMVC/DummyCache.php';
+
 ini_set('display_errors', '1');
 
-MiniMVC_Registry::getInstance()->settings = new MiniMVC_Settings('', '', false);
+MiniMVC_Registry::getInstance()->settings = new MiniMVC_Settings('', '', new MiniMVC_DummyCache());
 
 echo MiniMVC_Registry::getInstance()->task->dispatch($_SERVER['argv']) . "\n";
