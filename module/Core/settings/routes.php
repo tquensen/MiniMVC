8550a45f2962da02432fb1597ec4e5a5fe04567d
ScaraX <t.quensen@yeebase.com>
2010-12-23 23:28:29 +0100

    updated error pages

--- module/Core/settings/routes.php
+++ module/Core/settings/routes.php
@@ -1,30 +1,38 @@
 <?php
 $MiniMVC_routes['core.error401'] = array(
     'route' => 'error401',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error401',
 );
 $MiniMVC_routes['core.error403'] = array(
     'route' => 'error403',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error403',
 );
 $MiniMVC_routes['core.error404'] = array(
     'route' => 'error404',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error404',
 );
 $MiniMVC_routes['core.error404.debug'] = array(
+    'route' => 'error404debug',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error404',
     'parameter' => array('debug' => true)
 );
 $MiniMVC_routes['core.error500'] = array(
     'route' => 'error500',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error500',
 );
 $MiniMVC_routes['core.error500.debug'] = array(
+    'route' => 'error404debug',
+    'active' => false, //don't allow direct access
     'controller' => 'Core_Error',
     'action' => 'error500',
     'parameter' => array('debug' => true)
