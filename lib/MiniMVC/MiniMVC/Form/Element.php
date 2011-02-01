393e5e5756c368da659926b8965b91787deffe08
tquensen <t.quensen@yeebase.com>
2011-02-01 16:12:32 +0100

    updated form/form elements

--- lib/MiniMVC/MiniMVC/Form/Element.php
+++ lib/MiniMVC/MiniMVC/Form/Element.php
@@ -86,7 +86,7 @@ class MiniMVC_Form_Element
             if ($this->defaultValue !== null) {
                 $this->value = $this->defaultValue;
             } else {
-                if ($model = $this->getForm()->getModel()) {
+                if ($this->getOption('useModel') !== false && $model = $this->getForm()->getModel()) {
                     $property = $this->getOption('modelProperty') ? $this->getOption('modelProperty') : $this->name;
                     $this->value = $model->$property;
                 }
@@ -141,8 +141,10 @@ class MiniMVC_Form_Element
 	}
 
     public function updateModel($model) {
-        $property = $this->getOption('modelProperty') ? $this->getOption('modelProperty') : $this->name;
-        $model->$property = $this->value;
+        if ($this->getOption('useModel') !== false && $model = $this->getForm()->getModel()) {
+            $property = $this->getOption('modelProperty') ? $this->getOption('modelProperty') : $this->name;
+            $model->$property = $this->value;
+        }
     }
 
     /**
--- lib/MiniMVC/MiniMVC/Form/Element/Custom.php
+++ lib/MiniMVC/MiniMVC/Form/Element/Custom.php
@@ -6,14 +6,16 @@ class MiniMVC_Form_Element_Custom extends MiniMVC_Form_Element
     public function setValue($value)
 	{
         if ($this->getOption('setValueCallback') && is_callable($this->getOption('setValueCallback'))) {
-            call_user_func($this->getOption('setValueCallback'), $value);
+            call_user_func($this->getOption('setValueCallback'), $value, $this);
         }
 	}
 
 	public function updateModel($model)
 	{
-        if ($this->getOption('updateModelCallback') && is_callable($this->getOption('updateModelCallback'))) {
-            call_user_func($this->getOption('updateModelCallback'), $model);
+        if ($this->getOption('useModel') !== false && $model = $this->getForm()->getModel()) {
+            if ($this->getOption('updateModelCallback') && is_callable($this->getOption('updateModelCallback'))) {
+                call_user_func($this->getOption('updateModelCallback'), $model, $this);
+            }
         }
 	}
 }
