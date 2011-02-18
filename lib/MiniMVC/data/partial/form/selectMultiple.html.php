724fcec27e2cd38577691012a5a49ad7f9505d0f
ScaraX <t.quensen@yeebase.com>
2011-02-18 19:19:40 +0100

    updated forms

--- lib/MiniMVC/data/partial/form/selectMultiple.html.php
+++ lib/MiniMVC/data/partial/form/selectMultiple.html.php
@@ -2,7 +2,7 @@
         for="<?php echo htmlspecialchars($element->getForm()->getName() . '__' . $element->getName()) ?>"><?php echo htmlspecialchars($element->label) ?><?php if ($element->required && $element->getForm()->getOption('requiredMark')): ?><?php echo '<span class="requiredMark">'.htmlspecialchars($element->getForm()->getOption('requiredMark')).'</span>'; ?><?php endif; ?></label>
     <select
         <?php if ($element->attributes): foreach ((array) $element->attributes as $attr => $attrValue): ?> <?php echo ' '.$attr.'="'.$attrValue.'"'; ?><?php endforeach; endif; ?>
-        multiple="multiple" size="<?php echo htmlspecialchars($element->size ? $element->size : 8) ?>"
+        multiple="multiple"
         <?php if ($element->class): ?> class="<?php echo $element->class; ?>"<?php endif; ?>
         <?php if ($element->required): ?> required<?php endif; ?>
         name="<?php echo $element->forceName ? htmlspecialchars($element->forceName) : htmlspecialchars($element->getForm()->getName() . '[' . $element->getName() . ']') ?>[]"
